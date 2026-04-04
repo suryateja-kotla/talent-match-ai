ab1dbd43ffe90b581ba9f1fce585f53c6389791a
DeviTalabattula23 <devitalabattula23@gmail.com>
2026-04-04 12:13:25 +0530

    added user dasboard and login itegration

---
+++ frontend/src/app/components/user/job-matches/job-matches.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-matches',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-matches.component.html',
+  styleUrl: './job-matches.component.scss'
+})
+export class JobMatchesComponent {
+
+}
